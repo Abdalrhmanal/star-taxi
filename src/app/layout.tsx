34e8600c86d5a51a10e9ccbda51f06b3e25aff22
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-02-25 21:55:21 +0300

    Refactor RootLayout to simplify font handling and clean up imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
-import { Grid } from "@mui/material";
-import Navbar from "@/components/layout/navbar";
-import Sidebar from "@/components/side-bar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata = {
   title: {
@@ -33,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
 
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
         <AuthProvider>
           {children}
         </AuthProvider>
