f5b7e062e38e46d72280f8cdcef069afea72726a
abdulrahman_sr <homjpkatarhom6468@gmail.com>
2025-03-16 15:36:21 +0300

    increase height of GridItem in AppDownloadPage for improved layout

--- src/app/(auth)/platform/page.tsx
+++ src/app/(auth)/platform/page.tsx
@@ -235,7 +235,7 @@ function AppDownloadPage() {
       <Grid container spacing={3} sx={{ mb: 5 }}>
         {imageContents.map((content, index) => (
           <Grid item key={index} xs={12} sm={6} md={4}>
-            <GridItem elevation={2} sx={{ height: 600 ,p:1 }}>
+            <GridItem elevation={2} sx={{ height: 700 ,p:1 }}>
               <Box sx={{ position: "relative", width: "100%", height: "100%" }}>
                 <Image
                   src={`/images/${index + 1}.png`}
